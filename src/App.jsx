7a569063ef64d52de74dd4b6a17885ac3141c7e2
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-04 15:05:07 +0600

    gitHub link added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable no-unused-vars */
 
 import './App.css';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import app from './firebase/firebase.config';
 import { useState } from 'react';
 
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+const githubProvider=new GithubAuthProvider();
 
 function App() {
   const [userData, setUserData] = useState(null);
-
+  const [githubUserData, setgithubUserData] = useState(null);
+console.log(githubUserData);
   const handleGoogleSignin = () => {
     signInWithPopup(auth, googleProvider)
       .then((result) => {
@@ -21,13 +23,22 @@ function App() {
         console.log(err.message);
       });
   };
+  const handleGitHubSignin=() => {
+    signInWithPopup(auth, githubProvider)
+    .then((result) => {
+      const loggedUser = result.user;
+      setgithubUserData(loggedUser);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  };
 
   return (
     <>
       <h1>Firebase + React</h1>
-      <button onClick={handleGoogleSignin}>Google SignIn</button>
-      {/* Add a similar button and event handler for GitHub SignIn */}
-      {/* <button onClick={handleGitHubSignin}>GitHub SignIn</button> */}
+      <button style={{border:'1px solid gray'}} className='btn btn-neutral' onClick={handleGoogleSignin}>Google SignIn</button>
+      <button onClick={handleGitHubSignin}>GitHub SignIn</button>
       {userData && <h3>Name: {userData.displayName}</h3>}
     </>
   );
