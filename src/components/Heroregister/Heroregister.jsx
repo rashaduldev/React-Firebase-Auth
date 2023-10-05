a8a4b5ef7ebe193c1aad93d13d0739f431890b19
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-10-05 15:17:57 +0600

    Update Heroregister.jsx

--- src/components/Heroregister/Heroregister.jsx
+++ src/components/Heroregister/Heroregister.jsx
@@ -1,14 +1,41 @@
 // import React from 'react';
 
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+import { useState } from "react";
+
 
 const Heroregister = () => {
-const handleRegistration=e=>{
+
+  const [registerError,setRegisterError] =useState('');
+  const [success,setSuccess] = useState('');
+  console.log(registerError);
+  const handleRegistration=e=>{
     e.preventDefault();
     const email=e.target.email.value;
     const password=e.target.password.value;
     console.log(email, password);
-    console.log('heroregister');
+    setRegisterError('');
+
+    if (password.length <6 ) {
+      setRegisterError('Password should be at least 6 characters');
+      return;
+    } 
+
+    // Create user
+    createUserWithEmailAndPassword(auth,email,password)
+    .then(result=>{
+      console.log(result.user);
+      setSuccess("User created successfully");
+    })
+    .catch(err=>{
+      console.log(err);
+      console.log(err.message);
+      setRegisterError(err.message);
+    })
    
+    // console.log('heroregister');
+    // console.log(registerError);
 };
 
     return (
@@ -36,6 +63,15 @@ const handleRegistration=e=>{
           <button className="btn btn-primary">Login</button>
         </div>
       </form>
+      {  registerError && <div className="text-red-600 pb-5">
+          {registerError}
+        </div>
+      }
+      {
+        success && <div className="text-green-600 pb-5">
+          {success}
+        </div>
+      }
     </div>
   </div>
 </div>
